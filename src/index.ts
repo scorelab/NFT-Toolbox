e6343304861c9d02ae376dfdd10a4c707a5efdba
Sadashay Kanungo <sadashay01@gmail.com>
2022-07-25 02:50:05 +0530

    added Storj and Infura classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { PathLike } from "fs";
 import { Collection, LayerSchema } from "./classes/Collection";
 import { FileStorage } from "./classes/FileStorage";
+import { Infura } from "./classes/Infura";
+import { Storj } from "./classes/Storj";
 
 class Toolbox {
 	private collection: Collection | undefined = undefined;
@@ -31,6 +33,32 @@ class Toolbox {
 		wallet?: any;
 	}) {
 		switch (attr.service) {
+			case "storj":
+				if (!attr.username) {
+					throw new Error("STORJ Username required");
+				}
+				if (!attr.password) {
+					throw new Error("STORJ Password required");
+				}
+				this.fileStorageService = new Storj(
+					attr.username,
+					attr.password
+				);
+				break;
+
+			case "infura":
+				if (!attr.username) {
+					throw new Error("STORJ Username required");
+				}
+				if (!attr.password) {
+					throw new Error("STORJ Password required");
+				}
+				this.fileStorageService = new Infura(
+					attr.username,
+					attr.password
+				);
+				break;
+
 			default:
 				throw new Error("Unknown IPFS Service");
 		}
