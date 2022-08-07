b8a2b130962677911e461eb7b35c048f36ebab4b
Sadashay Kanungo <sadashay01@gmail.com>
2022-08-07 15:27:47 +0530

    added File and JSON upload to Arweave

--- src/classes/Arweave.ts
+++ src/classes/Arweave.ts
@@ -19,16 +19,8 @@ export class Arweave extends FileStorage {
 		// });
 	}
 
-	async uploadDirToService(dir: fs.PathLike) {
-		const dirSize = (directory: string) => {
-			const files = fs.readdirSync(directory);
-			const stats = files.map((file) =>
-				fs.statSync(path.join(directory.toString(), file))
-			);
-			return stats.reduce((total, { size }) => total + size, 0);
-		};
-
-		const price = await this.CONNECTION.getPrice(dirSize(dir.toString()));
+	async fundBundlr(dataSize: number) {
+		const price = await this.CONNECTION.getPrice(dataSize);
 		const balance = await this.CONNECTION.getLoadedBalance();
 
 		if (price.multipliedBy(1.1).isGreaterThan(balance)) {
@@ -38,6 +30,18 @@ export class Arweave extends FileStorage {
 				price.multipliedBy(1.1).minus(balance).integerValue()
 			);
 		}
+	}
+
+	async uploadDirToService(dir: fs.PathLike) {
+		const dirSize = (directory: string) => {
+			const files = fs.readdirSync(directory);
+			const stats = files.map((file) =>
+				fs.statSync(path.join(directory.toString(), file))
+			);
+			return stats.reduce((total, { size }) => total + size, 0);
+		};
+
+		this.fundBundlr(dirSize(dir.toString()));
 
 		const response = await this.CONNECTION.uploader.uploadFolder(
 			dir.toString()
@@ -45,5 +49,26 @@ export class Arweave extends FileStorage {
 		//returns the manifest ID if successful.
 
 		return response;
+	}
+
+	async uploadFileToService(file: fs.PathLike) {
+		this.fundBundlr(fs.statSync(file).size);
+
+		const response = await this.CONNECTION.uploader.uploadFile(
+			file.toString()
+		);
+		//returns the manifest ID if successful.
+
+		return response.data;
+	}
+
+	async uploadJSONToService(json: string) {
+		const data = Buffer.from(json);
+		this.fundBundlr(data.byteLength);
+
+		const response = await this.CONNECTION.uploader.upload(data);
+		//returns the manifest ID if successful.
+
+		return response.data;
 	}
 }
